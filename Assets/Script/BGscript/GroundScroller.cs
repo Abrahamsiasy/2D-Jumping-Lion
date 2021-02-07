25ec67598674e419f72e8a4d80b01dda8b64514a
Abrahamsiasy <abrahamsisaysis@gmail.com>
2021-02-07 05:26:52 +0000

    Background and ground Scroller added

---
+++ Assets/Script/BGscript/GroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScroller : MonoBehaviour
+{
+    public float groundSpeed = 0.1f;
+    private Vector2 offset;
+    private new Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(Time.time * groundSpeed, 0);
+        renderer.material.mainTextureOffset = offset;
+    }
+}
