59b6d54d2d1ba5e977f3df9355985860a9816795
Abrahamsiasy <abrahamsisaysis@gmail.com>
2021-02-07 05:10:23 +0000

    Background Scroller added

---
+++ Assets/Script/BGscript/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    public float speed = -0.04f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the meshrenderer object from the game
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the material property anf the texture offset so it can hel it move
+        // use the new vector2 then add the speed to move on x direction by mjltiplying it with delta time 
+        meshRenderer.material.mainTextureOffset -= new Vector2(speed * Time.deltaTime, 0);
+    }
+}
